899ffc0497ade99a180e80003987c3fbd7fcbf71
小仙女 <me@ilyfairy.com>
2022-04-23 15:08:18 +0800

    添加Demo

---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using IlyfairyLib.GoCqHttpSdk;
+using IlyfairyLib.GoCqHttpSdk.Api;
+
+Session session = new("ws://127.0.0.1:6700", "http://127.0.0.1:5700");
+
+//接收所有群消息
+session.UseGroupMessage(async v =>
+{
+    Console.WriteLine($"消息json: \n{v.Message.ToJson()}");
+    return true; //继续向下传递
+});
+
+//复读机示例
+session.MapGroup(@"^echo\s*(?<content>.*)$", async (v, group) =>
+{
+    await session.SendRawGroupMessageAsync(v.GroupId, group["content"].Value);
+});
+
+session.Build();
+
+new AutoResetEvent(false).WaitOne();

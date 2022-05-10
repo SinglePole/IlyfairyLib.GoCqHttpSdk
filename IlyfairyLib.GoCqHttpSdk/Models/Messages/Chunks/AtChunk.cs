22a686ef4130af5ce58b3402131617117c0618db
小仙女 <me@ilyfairy.com>
2022-05-10 16:17:06 +0800

    修改了AtChunk

--- IlyfairyLib.GoCqHttpSdk/Models/Messages/Chunks/AtChunk.cs
+++ IlyfairyLib.GoCqHttpSdk/Models/Messages/Chunks/AtChunk.cs
@@ -11,29 +11,46 @@ public sealed class AtChunk : MessageChunk
 
     private long qq;
 
+    /// <summary>
+    /// 被艾特的QQ
+    /// </summary>
     public long QQ
     {
         get { return qq; }
         set { qq = value; Data["qq"] = value; }
     }
 
+    /// <summary>
+    /// 初始化艾特消息块
+    /// </summary>
+    /// <param name="qq">要艾特的qq</param>
     public AtChunk(long qq)
     {
+        if (qq < 0) qq = 0;
         QQ = qq;
     }
-
-    internal AtChunk(bool isAll)
+    internal AtChunk()
     {
-        if (isAll)
+    }
+
+    /// <summary>
+    /// 艾特所有人
+    /// </summary>
+    public static AtChunk All
+    {
+        get
         {
-            qq = -1;
-            this.Data["qq"] = "all";
+            AtChunk at = new();
+            at.qq = -1;
+            at.Data["qq"] = "all";
+            return at;
         }
     }
 
+    /// <summary>
+    /// 是否艾特所有人
+    /// </summary>
     public bool IsAtAll { get => qq is -1; }
-
-    public static AtChunk All => new(true);
 
     public static new AtChunk? Parse(JToken json)
     {
@@ -43,6 +60,7 @@ public sealed class AtChunk : MessageChunk
             if (qq == "all")
             {
                 result = All;
+                result.QQ = -1;
             }
             else
             {
@@ -51,5 +69,27 @@ public sealed class AtChunk : MessageChunk
             result.Data = json as JObject;
         }
         return result;
+    }
+
+    /// <summary>
+    /// 判断两个at是否相等
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static bool operator ==(AtChunk left, AtChunk right)
+    {
+        return left.qq == right.QQ;
+    }
+
+    /// <summary>
+    /// 判断两个at是否不相等
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static bool operator !=(AtChunk left, AtChunk right)
+    {
+        return !(left == right);
     }
 }

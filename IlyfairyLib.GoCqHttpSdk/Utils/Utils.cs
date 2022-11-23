d200d1fddf18f5cf33804160bb1f9a65ad6eb964
小仙女 <me@ilyfairy.com>
2022-11-23 20:22:54 +0800

    增加了一些没有用的特性

--- IlyfairyLib.GoCqHttpSdk/Utils/Utils.cs
+++ IlyfairyLib.GoCqHttpSdk/Utils/Utils.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IlyfairyLib.GoCqHttpSdk.Utils
 {
+    /// <summary>
+    /// 工具扩展
+    /// </summary>
     public static class UtilsExtentsion
     {
+        // , &#44;
         // & &amp;
         // [ &#91;
         // ] &#93;
 
-
-        public static string? ToText(this string? rawText)
+        [return: NotNullIfNotNull(nameof(rawText))]
+        public static string? ToText(this string rawText)
         {
             if (rawText is null) return null;
             StringBuilder s = new(rawText);
@@ -23,7 +28,8 @@ namespace IlyfairyLib.GoCqHttpSdk.Utils
             return s.ToString();
         }
 
-        public static string? ToRawText(this string? text)
+        [return: NotNullIfNotNull(nameof(text))]
+        public static string? ToRawText(this string text)
         {
             if (text is null) return null;
             StringBuilder s = new(text);
@@ -33,7 +39,8 @@ namespace IlyfairyLib.GoCqHttpSdk.Utils
             return s.ToString();
         }
 
-        public static string? ToJsonRawText(this string? jsonText)
+        [return: NotNullIfNotNull(nameof(jsonText))]
+        public static string? ToJsonRawText(this string jsonText)
         {
             if (jsonText is null) return null;
             StringBuilder s = new(jsonText);
@@ -44,7 +51,8 @@ namespace IlyfairyLib.GoCqHttpSdk.Utils
             return s.ToString();
         }
 
-        public static string? ToJsonText(this string? jsonRawText)
+        [return: NotNullIfNotNull(nameof(jsonRawText))]
+        public static string? ToJsonText(this string jsonRawText)
         {
             if (jsonRawText is null) return null;
             StringBuilder s = new(jsonRawText);

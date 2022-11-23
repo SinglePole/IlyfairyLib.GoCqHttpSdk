d29e603a1e80ff301e1be56c93996bd09653f844
小仙女 <me@ilyfairy.com>
2022-11-23 20:40:05 +0800

    优化字符串替换

--- IlyfairyLib.GoCqHttpSdk/Utils/Utils.cs
+++ IlyfairyLib.GoCqHttpSdk/Utils/Utils.cs
@@ -5,62 +5,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IlyfairyLib.GoCqHttpSdk.Utils
+namespace IlyfairyLib.GoCqHttpSdk.Utils;
+
+/// <summary>
+/// 工具扩展
+/// </summary>
+public static class UtilsExtentsion
 {
-    /// <summary>
-    /// 工具扩展
-    /// </summary>
-    public static class UtilsExtentsion
+    // , &#44;
+    // & &amp;
+    // [ &#91;
+    // ] &#93;
+    internal static Dictionary<string, string> RawToText = new()
     {
-        // , &#44;
-        // & &amp;
-        // [ &#91;
-        // ] &#93;
+        { "&amp;", "&" },
+        { "&#91;", "[" },
+        { "&#93;", "]" },
+    };
+    internal static Dictionary<string, string> TextToRaw = new()
+    {
+        { "&", "&amp;" },
+        { "[", "&#91;" },
+        { "]", "&#93;" },
+    };
+    internal static Dictionary<string, string> JsonToRaw = new()
+    {
+        { ",", "&#44;" },
+        { "&", "&amp;" },
+        { "[", "&#91;" },
+        { "]", "&#93;" },
+    };
+    internal static Dictionary<string, string> RawToJson = new()
+    {
+        { "&#44;", "," },
+        { "&amp;", "&" },
+        { "&#91;", "[" },
+        { "&#93;", "]" },
+    };
 
-        [return: NotNullIfNotNull(nameof(rawText))]
-        public static string? ToText(this string rawText)
-        {
-            if (rawText is null) return null;
-            StringBuilder s = new(rawText);
-            s.Replace("&amp;", "&");
-            s.Replace("&#91;", "[");
-            s.Replace("&#93;", "]");
-            return s.ToString();
-        }
+    [return: NotNullIfNotNull(nameof(rawText))]
+    public static string? ToText(this string rawText)
+        => rawText?.ReplaceAll(RawToText);
 
-        [return: NotNullIfNotNull(nameof(text))]
-        public static string? ToRawText(this string text)
-        {
-            if (text is null) return null;
-            StringBuilder s = new(text);
-            s.Replace("&", "&amp;");
-            s.Replace("[", "&#91;");
-            s.Replace("]", "&#93;");
-            return s.ToString();
-        }
+    [return: NotNullIfNotNull(nameof(text))]
+    public static string? ToRawText(this string text)
+        => text?.ReplaceAll(TextToRaw);
 
-        [return: NotNullIfNotNull(nameof(jsonText))]
-        public static string? ToJsonRawText(this string jsonText)
-        {
-            if (jsonText is null) return null;
-            StringBuilder s = new(jsonText);
-            s.Replace(",", "&#44;");
-            s.Replace("&", "&amp;");
-            s.Replace("[", "&#91;");
-            s.Replace("]", "&#93;");
-            return s.ToString();
-        }
+    [return: NotNullIfNotNull(nameof(jsonText))]
+    public static string? ToJsonRawText(this string jsonText)
+        => jsonText?.ReplaceAll(JsonToRaw);
 
-        [return: NotNullIfNotNull(nameof(jsonRawText))]
-        public static string? ToJsonText(this string jsonRawText)
+    [return: NotNullIfNotNull(nameof(jsonRawText))]
+    public static string? ToJsonText(this string jsonRawText)
+        => jsonRawText.ReplaceAll(RawToJson);
+
+    internal static string ReplaceAll(this string source, Dictionary<string, string> replacements)
+    {
+        bool StringEquals(string source1, string source2, int offset1, int offset2, int count)
         {
-            if (jsonRawText is null) return null;
-            StringBuilder s = new(jsonRawText);
-            s.Replace("&#44;", ",");
-            s.Replace("&amp;", "&");
-            s.Replace("&#91;", "[");
-            s.Replace("&#93;", "]");
-            return s.ToString();
+            for (int i = 0; i < count; i++)
+            {
+                if (source1[offset1 + i] != source2[offset2 + i])
+                    return false;
+            }
+            return true;
         }
+        StringBuilder s = new();
+        for (int i = 0; i < source.Length;)
+        {
+            bool matched = false;
+            do
+            {
+                foreach (var kv in replacements)
+                {
+                    if (i >= source.Length)
+                        break;
+                    int count = Math.Min(source.Length - i, kv.Key.Length);
+                    if (StringEquals(source, kv.Key, i, 0, count))
+                    {
+                        matched |= true;
+                        s.Append(kv.Value);
+                        i += kv.Key.Length;
+                    }
+                }
+            }
+            while (matched && i < source.Length);
+            if (!matched)
+                i++;
+        }
+        return s.ToString();
     }
 }
